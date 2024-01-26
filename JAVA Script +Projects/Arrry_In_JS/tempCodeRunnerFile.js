01ea7b147274b3de564c69d985bb81e9d60e3ec2
Srijit Bera <berasrijit02@gmail.com>
2024-01-26 12:07:40 +0530

    Delete JAVA Script +Projects directory

--- JAVA Script +Projects/Arrry_In_JS/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-const Array=[
-      ['arr1','arr2'],
-      ['arra3','arra4'],
-      ['arra5','arra6']
-];
-let flatArr=Array.reduce((accu,curVal)=>{
-      return accu.concat(curVal);
-});
-console.log(flatArr);
