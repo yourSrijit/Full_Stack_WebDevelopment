3b286ca0013530927f966061be406142631b5c1d
yourSrijit <berasrijit02@gmail.com>
2024-03-14 13:14:37 +0530

    js interview question

--- 3_JavaScript +Projects/JavaScript Interview Question/README.md
+++ 3_JavaScript +Projects/JavaScript Interview Question/README.md
@@ -1,11 +1,12 @@
 ## 1 queston
 ```
-console.log('Value of age is ' ,age);
+console.log('Value of age is ' ,age); //undefined
 var age=20;
-console.log('Value of age is ' ,age);
+console.log('Value of age is ' ,age);  //20
 ```
 Create a Global execution context .It has two part Memory phase and Code phase 
-<!-- #region drawnote -->
-<svg id="svg" xmlns="http://www.w3.org/2000/svg" viewbox="186.74000549316406,34,407,244" style="height:244"><rect x="196.74" y="50" fill="none" stroke="#6190e8" stroke-width="2" width="387" height="216" d="M 196.74 50 h 387 v 216 h -387 Z"></rect><text font-family="inherit" font-size="14" fill="#6190e8" x="216.74" y="64">Memory Phase</text><text font-family="inherit" font-size="14" fill="#6190e8" x="418.74" y="59">Code Phase</text><line x1="399.74" y1="49" x2="401.74" y2="268" fill="none" stroke="#6190e8" stroke-width="2" d="M 399.74 49 L 401.74 268"></line></svg>  
-<!-- #endregion -->
 
+
+## 2 Q Temporal Dead Zone
+If we create a variable using let or const asn try to access before initializaton a Temporal Dead zone is created .But in case of var it is not created.
+If we try to access a variable before initializaton temporal dead zone is created
--- 3_JavaScript +Projects/JavaScript Interview Question/app.js
+++ 3_JavaScript +Projects/JavaScript Interview Question/app.js
@@ -1,4 +1,75 @@
-// 1
-console.log('Value of age is ' ,age);
-var age=20;
-console.log('Value of age is ' ,age);
+// 1 question
+// console.log('Value of age is ' ,age);
+// var age=20;
+// console.log('Value of age is ' ,age);
+
+// 2 question
+
+// age=100;
+// console.log('Value of age is ' ,age); //100
+// var age=20;
+// console.log('Value of age is ' ,age);  //20
+
+
+// 3 question
+
+// myFun();
+// var myFun =function(){
+//     console.log('First');
+// }
+// myFun();
+// function myFun(){
+//     console.log('Second');
+// }
+// myFun();
+// ------
+// Second
+// First
+// First
+
+// 4 question
+    // var variable=10;
+    // (()=>{
+    //     console.log(variable);
+    //     variable=20;
+    //     console.log(variable);
+    // })(); //iife
+    // console.log(variable);
+    // var variable=30;
+    // ----------
+    // 10
+    // 20
+    // 20
+
+// 5 question
+
+// var variable=10;
+// (()=>{
+//     fo=100;
+//     console.log(variable);
+//     var fo=120
+//     variable=20;
+//     console.log(variable);
+// })(); //iife
+// console.log(fo);
+// console.log(variable);
+// var variable=30;
+
+// 6 question
+  for(var i=0;i<10;i++){
+    setTimeout(()=>{
+        console.log(i);
+    },0)
+  }
+// all 10 for var as var is global
+// 0 to 10 for let as let is local
+
+
+// 1 question
+// 1 question
+
+// 1 question
+
+// 1 question
+
+// 1 question
